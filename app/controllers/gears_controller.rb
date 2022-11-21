67c8afd7b471eeac03ce476d24972de5cbebb319
Ambery Harris <amberyharris.ah@gmail.com>
2022-11-21 17:26:30 +1100

    Made Devise_User model, Gears model and controllers and views (index, new, create)

---
+++ app/controllers/gears_controller.rb
@@ -0,0 +1,29 @@
+class GearsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    @gears = Gear.all
+  end
+
+  def new
+    @user = current_user
+    @gear = Gear.new
+  end
+
+  def create
+    @user = current_user
+    @gear = Gear.new(gear_params)
+    @gear.user = @user
+    if @gear.save
+      redirect_to root_path, status: :see_other
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def gear_params
+    params.require(:gear).permit(:gear_type, :total_occupancy, :summary, :address, :price, :latitude, :longitude)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  resources :users do
+    resources :gears, only: [:new, :create]
+  end
+  resources :gears, only: :index
 end
