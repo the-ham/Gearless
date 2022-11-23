5e6b7c32c2c2cd2c866e4ad872761f91877709b3
Elliott Kan <elliottkan7@gmail.com>
2022-11-23 11:29:08 +1100

    dashboard functionality started

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_011710) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "rentals", force: :cascade do |t|
     t.float "price"
