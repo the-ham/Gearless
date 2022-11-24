7b17cb0ca01810e6ddb6975009a30a32fce904f2
Caaasey <baimaxsolution@gmail.com>
2022-11-25 10:46:21 +1100

    completed my rental page

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,13 +5,16 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
-  static targets = [ "startTime", "endTime" ]
+  static targets = [ "startTime", "endTime"]
 
   connect() {
     console.log("date connected")
     const today = new Date()
     const tomorrow = new Date(today)
     tomorrow.setDate(tomorrow.getDate() + 1)
+    const totalPrice = document.querySelector("#totalprice");
+    const priceStr = document.querySelector("#price").innerText;
+    const price = priceStr / 1;
     flatpickr(this.startTimeTarget, {
               // enableTime: true,
               altInput: true,
@@ -19,7 +22,15 @@ export default class extends Controller {
               dateFormat: "Y-m-d",
               minDate: tomorrow,
               // Provide an id for the plugin to work
-              plugins: [new rangePlugin({ input: "#end_time"})]})
+              plugins: [new rangePlugin({ input: "#end_time"})],
+              onChange: function(selectedDates) {
+                console.log();
+                if(selectedDates.length === 2){
+                  let timeDiff = Math.abs(selectedDates[1].getDate() - selectedDates[0].getDate());
+                  totalPrice.innerHTML = `${timeDiff * price}`
+                }
+            }
+            })
     flatpickr(this.endTimeTarget, {})
   }
 }
